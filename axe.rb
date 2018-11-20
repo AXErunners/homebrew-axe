2eeaa93afcd8237619be22cc183bfb0b8654df83
-k <slowdive@me.com>
2018-11-19 20:39:37 -0500

    add AXE formula

---
+++ axe.rb
@@ -0,0 +1,63 @@
+class Bitcoin < Formula
+  desc "Decentralized, peer to peer payment network"
+  homepage "https://axerunners.com/"
+  url "https://bitcoin.org/bin/bitcoin-core-0.17.0/bitcoin-0.17.0.tar.gz"
+  revision 1
+
+  head do
+    url "https://github.com/axerunners/axe.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "berkeley-db@4"
+  depends_on "boost"
+  depends_on "libevent"
+  depends_on "miniupnpc"
+  depends_on "openssl"
+  depends_on "zeromq"
+
+  needs :cxx11
+
+  def install
+    if MacOS.version == :el_capitan && MacOS::Xcode.installed? &&
+       MacOS::Xcode.version >= "8.0"
+      ENV.delete("SDKROOT")
+    end
+
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-boost-libdir=#{Formula["boost"].opt_lib}",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    pkgshare.install "share/rpcauth"
+  end
+
+  plist_options :manual => "axed"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/axed</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system "#{bin}/test_axe"
+  end
+end
