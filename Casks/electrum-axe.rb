ee59e1a81ad3f98b943a3695dd5103f4756823d6
-k <slowdive@me.com>
2018-11-22 21:30:25 -0500

    add checksum

--- Casks/electrum-axe.rb
+++ Casks/electrum-axe.rb
@@ -1,5 +1,6 @@
 cask 'electrum-axe' do
   version '3.2.3.1'
+  sha256 '224bc4e231e69a743b1c2b246db6d7860abed2d3a124d1f5d5e8578eb027a8a1'
 
   url "https://github.com/AXErunners/electrum-axe/releases/download/v#{version}/electrum-axe-#{version}-macosx.dmg"
   appcast 'https://github.com/AXErunners/electrum-axe/releases.atom'
