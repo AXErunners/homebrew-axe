7fe986af0633fcdd8e0a57c95d18e2a2db244b59
-k <slowdive@me.com>
2019-01-24 01:11:07 -0800

    update core cask
    v1.2.0

--- Casks/axe.rb
+++ Casks/axe.rb
@@ -1,15 +1,14 @@
 cask 'axe' do
-  version '1.1.8'
-  sha256 'bb76e5d20f7db4e17f445e8b708b64429946a6928b7738ea9c99cbeca864259e'
+  version '1.2.0'
 
   url "https://github.com/axerunners/axe/releases/download/v#{version}/axecore-#{version}-osx-unsigned.dmg"
   appcast 'https://github.com/axerunners/axe/releases.atom'
   name 'AXE'
   homepage 'https://github.com/axerunners/axe'
 
-  app 'AXE-Qt.app'
+  app 'Axe-Qt.app'
 
   preflight do
-    set_permissions "#{staged_path}/AXE-Qt.app", '0755'
+    set_permissions "#{staged_path}/Axe-Qt.app", '0755'
   end
 end
