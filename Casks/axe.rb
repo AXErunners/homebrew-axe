d55ebb80f09f7ca1c053fba48f9355a7c3f71fdb
-k <slowdive@me.com>
2019-01-24 01:43:17 -0800

    add checksum

--- Casks/axe.rb
+++ Casks/axe.rb
@@ -1,6 +1,6 @@
 cask 'axe' do
   version '1.2.0'
-
+  sha256 '14d9fa840cfe841c96dc67fdd620bbb50327995e174450c6a51e351c61c9d17d'
   url "https://github.com/axerunners/axe/releases/download/v#{version}/axecore-#{version}-osx-unsigned.dmg"
   appcast 'https://github.com/axerunners/axe/releases.atom'
   name 'AXE'
