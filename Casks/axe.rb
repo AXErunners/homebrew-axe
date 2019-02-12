c429bf7c432ae50afa18cbb7ac9f2f2d532f1b43
-k <slowdive@me.com>
2019-02-12 09:54:37 -0800

    Update axe.rb

--- Casks/axe.rb
+++ Casks/axe.rb
@@ -3,7 +3,7 @@ cask 'axe' do
   sha256 '14d9fa840cfe841c96dc67fdd620bbb50327995e174450c6a51e351c61c9d17d'
   url "https://github.com/axerunners/axe/releases/download/v#{version}/axecore-#{version}-osx-unsigned.dmg"
   appcast 'https://github.com/axerunners/axe/releases.atom'
-  name 'AXE'
+  name 'Axe'
   homepage 'https://github.com/axerunners/axe'
 
   app 'Axe-Qt.app'
