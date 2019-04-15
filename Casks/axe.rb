2698a9d156d3d668dcf6824852e2070310e11f16
-k <slowdive@me.com>
2019-04-14 18:53:03 -0700

    axe core 1.3.0

--- Casks/axe.rb
+++ Casks/axe.rb
@@ -1,6 +1,6 @@
 cask 'axe' do
-  version '1.2.1'
-  sha256 '0db5a916765badc752d78d280e011d5b04051bfc778363c5d6e13679fde24251'
+  version '1.3.0'
+  sha256 'af2a76d147e20c6fd2b466e75c03681aef7c40c89d2296d4ff5189a0e6f09389'
   url "https://github.com/axerunners/axe/releases/download/v#{version}/axecore-#{version}-osx-unsigned.dmg"
   appcast 'https://github.com/axerunners/axe/releases.atom'
   name 'Axe'
