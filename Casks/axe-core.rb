a44ca260952d27bb2d8ecaa9925ae6dd49b011c8
-k <slowdive@me.com>
2019-04-15 16:39:17 -0700

    fix casks style

--- Casks/axe-core.rb
+++ Casks/axe-core.rb
@@ -2,15 +2,16 @@ cask 'axe-core' do
   version '1.3.0'
   sha256 'af2a76d147e20c6fd2b466e75c03681aef7c40c89d2296d4ff5189a0e6f09389'
 
-  # axerunners.github.io was verified as official when first introduced to the cask
+  # github.com/axerunners/axe was verified as official when first introduced to the cask
   url "https://github.com/axerunners/axe/releases/download/v#{version}/axecore-#{version}-osx-unsigned.dmg"
   appcast 'https://github.com/axerunners/axe/releases.atom'
   name 'Axe'
-  homepage 'https://axerunners.github.io/'
+  homepage 'https://github.com/axerunners/axe'
 
   app 'Axe-Qt.app', target: 'Axe Core.app'
 
   preflight do
     set_permissions "#{staged_path}/Axe-Qt.app", '0755'
   end
+
 end
